2ab2c441c5c68e7cccc6038d51f0e77524d8031d
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-21 18:10:19 -0300

    create project

--- front-project/src/App.vue
+++ front-project/src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <header>
-    
-  </header>
+  <Header></Header>
   
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '../src/components/Header.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header
+  }
+})
+</script>
 
---
+++ front-project/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div>
+            <img src="./icons/logo.png" alt="Foodo Delivery Logo">
+            <h1>Foodo Delivery</h1>
+        </div>
+        <div>
+            <v-text-field label="Search for restaurant..."></v-text-field>
+            <v-btn size="large" color="red-darken-1">Search</v-btn>
+        </div>
+        <div>
+            <v-icon color="success" icon="mdi-information" size="large"></v-icon>
+            My Account
+            Sign in - Register
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "Header"
+})
+</script>
+
