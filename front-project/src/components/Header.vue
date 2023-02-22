2bd9e599543e71ddd51949ec5985e088e30e3878
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-21 22:21:42 -0300

    created header

--- front-project/src/components/Header.vue
+++ front-project/src/components/Header.vue
@@ -1,19 +1,27 @@
 <template>
-    <header>
-        <div>
-            <img src="./icons/logo.png" alt="Foodo Delivery Logo">
-            <h1>Foodo Delivery</h1>
-        </div>
-        <div>
-            <v-text-field label="Search for restaurant..."></v-text-field>
-            <v-btn size="large" color="red-darken-1">Search</v-btn>
-        </div>
-        <div>
-            <v-icon color="success" icon="mdi-information" size="large"></v-icon>
-            My Account
-            Sign in - Register
-        </div>
-    </header>
+    <v-container>
+        <v-toolbar color="white" class="py-2" height="100px" cols="6" offset="3">
+            <v-row justify="space-between" align="center">
+                <v-col md="3" class="d-flex justify-center align-end">
+                    <img src="./icons/logo.png" alt="Foodo Delivery Logo">
+                    <h1 class="text-h3"><small class="text-h2">F</small>oodo</h1>
+                </v-col>
+                
+                <v-col md="6" class="d-flex justify-center align-center">
+                    <v-text-field label="Search for restaurant..." hide-details class="mr-2" append-inner-icon="mdi-magnify" variant="solo" density="compact"></v-text-field>                    
+                    <v-btn variant="elevated" color="red-darken-1">Search</v-btn>                    
+                </v-col>
+                
+                <v-col md="3" class="d-flex justify-center align-center">
+                    <v-icon color="red-darken-1 mr-2" icon="mdi-account-outline" size="x-large"></v-icon>
+                    <div class="d-flex flex-column">
+                        <v-btn href="#" variant="flat" size="small" class="link px-0">My Account</v-btn>
+                        <v-btn href="#" variant="flat" size="small" class="link px-0">Sign in - Register</v-btn>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-toolbar>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -23,4 +31,28 @@ export default defineComponent({
     name: "Header"
 })
 </script>
+
+<style scoped>
+@import '../assets/base.css';
+
+img {
+    width: 70px;
+}
+
+h1 {
+    font-family: 'Sacramento', cursive !important;
+    font-weight: 550;    
+}
+
+small {
+    font-family: 'Sacramento', cursive !important;
+    color: #E53935;
+    font-weight: 900;
+}
+
+.link {
+    color: --vt-c-indigo !important;
+    --v-btn-height: 20px;
+}
+</style>
 
--- front-project/src/plugins/vuetify.js
+++ front-project/src/plugins/vuetify.js
@@ -1,12 +1,8 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default createVuetify({
   icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    }
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
   },
 })
