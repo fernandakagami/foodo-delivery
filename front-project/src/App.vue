b969534979efe5f31011ea074f1c059ec0c4e197
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-23 10:21:28 -0300

    Update App.vue

--- front-project/src/App.vue
+++ front-project/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Header></Header>
-  <RouterView></RouterView>
+  <NavBar></NavBar>
+  <RouterView />
   <Footer></Footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from '../src/components/Header.vue'
+import NavBar from '../src/components/NavBar.vue'
 import Footer from '../src/components/Footer.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
+    NavBar,
     Footer
   }
 })
