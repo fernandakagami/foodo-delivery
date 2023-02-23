7d4bd2590656a737a59e201bfead3bc6dc225763
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-23 10:21:09 -0300

    Update HomeView.vue

--- front-project/src/views/HomeView.vue
+++ front-project/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
-  <main>
-    oi
-  </main>
+  <BannerCarousel></BannerCarousel>  
+    <v-container class="d-flex flex-column justify-center align-center">      
+      oi 
+    </v-container>    
+  
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
+import BannerCarousel from '../../src/components/BannerCarousel.vue'
 
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    BannerCarousel
+  }
+
+})
 </script>
+
